d4f251826e5103b56a80a0528bdd514f3ccff739
iraytee-code <adeniranhope21@gmail.com>
2023-01-15 21:30:53 +0100

    set up projrect folder

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,21 @@
+import { TextInput, View } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    marginTop: 100,
+    padding: 16,
+  },
+});
